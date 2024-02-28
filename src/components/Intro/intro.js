b88259b043e2b57f9f78a86bc5f276bfabd0793e
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-02-29 01:38:08 +0530

    Added Intro

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './intro.css';
+import bg from  "../../assets/IMG_31311.png";
+import btnImg from "../../assets/hireme.png"
+import { Link } from 'react-scroll';
+function Intro() {
+  return (
+    <section id='intro'>
+        <div className='introContent'>
+            <span className='hello'>Hello</span>
+            <span className='introText'>I'm <span className='introName'> Raghav</span> <br /> Website Designer</span>
+            <p className='introPara'>I am a skilled Web Developer with a keen interest in creating <br /> and developing visually appealing websites.</p>
+            <Link><button className='btn'><img src={btnImg} alt='Hire' className='btnImg' />Hire Me!</button></Link>
+        </div>
+        <img src={bg} alt="Profile" className="bg" />
+    </section>
+  )
+}
+
+export default Intro
