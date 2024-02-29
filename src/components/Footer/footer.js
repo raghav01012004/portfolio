0871b19e49c1303b6fda4dc9e7148b3f5e1d59d8
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-03-01 02:22:41 +0530

    Added Mobile Menu

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+        Copyright &#169; 2024 Raghav Dixit. All rights received.
+    </footer>
+  )
+}
+
+export default Footer
