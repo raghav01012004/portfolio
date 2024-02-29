de0818a263b4cf9a06e10d85233cc9549f4e064f
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-03-01 00:53:21 +0530

    Added Works

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './works.css'
+import Portfolio1 from '../../assets/portfolio-1.png';
+import Portfolio2 from '../../assets/portfolio-2.png';
+import Portfolio3 from '../../assets/portfolio-3.png';
+import Portfolio4 from '../../assets/portfolio-4.png';
+import Portfolio5 from '../../assets/portfolio-5.png';
+import Portfolio6 from '../../assets/portfolio-6.png';
 
 function Works() {
   return (
     <section id = 'works'>
-        Work Done
+        <h2 className='worksTitle'>My Portfolio</h2>
+       <span className='worksDesc'>I take pride in paying attention to the smallest details and making sure that the work is pixel perfect. I am excited to bring my skills and experience to help business achieve their goals and create a strong online presence.</span>
+       <div className='worksImgs'>
+        <img src={Portfolio1} alt="project one" className='worksImg'/>
+        <img src={Portfolio2} alt="project two" className='worksImg'/>
+        <img src={Portfolio6} alt="project three" className='worksImg'/>
+        <img src={Portfolio4} alt="project four" className='worksImg'/>
+        <img src={Portfolio3} alt="project five" className='worksImg'/>
+        <img src={Portfolio5} alt="project six" className='worksImg'/>
+       </div>
+       <button className='worksBtn'>See More</button>
     </section>
   )
 }
